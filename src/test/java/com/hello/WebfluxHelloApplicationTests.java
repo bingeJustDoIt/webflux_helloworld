9063fb4781835b5dfe7c1ddec9278cff9109058c
Binge <826819173@qq.com>
2023-04-24 16:11:00 +0800

    first

---
+++ src/test/java/com/hello/WebfluxHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
